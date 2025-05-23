99fcde156ed6bf24fd9c804710dc017e6c1393bf
daran6255 <darandharani6255@gmail.com>
2025-05-23 09:54:54 +0530

    static route added

---
+++ backend/app/api/static_routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, send_from_directory, current_app
+import os
+
+static_bp = Blueprint("static_bp", __name__)
+
+@static_bp.route("/static/pdfs/<path:filename>")
+def serve_pdf(filename):
+    pdf_dir = os.path.join(current_app.root_path, "..", "static", "pdfs")
+    return send_from_directory(pdf_dir, filename)
+
+@static_bp.route("/static/images/<path:filename>")
+def serve_image(filename):
+    image_dir = os.path.join(current_app.root_path, "..", "static", "images")
+    return send_from_directory(image_dir, filename)
